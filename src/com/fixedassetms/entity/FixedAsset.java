fab1138305dd5964f00398056f5b8bd5c43722e2
Muse_dreame <450403579@qq.com>
2016-07-15 16:32:28 +0800

    fixedasset

--- src/com/fixedassetms/entity/FixedAsset.java
+++ src/com/fixedassetms/entity/FixedAsset.java
@@ -1,5 +1,104 @@
 package com.fixedassetms.entity;
 
+import java.util.Date;
+
 public class FixedAsset {
+	
+	/**
+	 * 设备编号
+	 */
+	private int id;
+	/**
+	 * 名称
+	 */
+	private String name;
+	/**
+	 * 类别
+	 */
+	private String category;
+	/**
+	 * 型号
+	 */
+	private String type;
+	/**
+	 * 价值
+	 */
+	private int price;
+	/**
+	 * 购买日期(xxxx-xx-xx)
+	 */
+	private Date indate;
+	/**
+	 * 状态（正常、维修、报废）
+	 */
+	private String status;
+	/**
+	 * 使用者
+	 */
+	
+	private String user;
+	/**
+	 * 备注
+	 */
+	private String remark;
+	
+	
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public Date getIndate() {
+		return indate;
+	}
+	public void setIndate(Date indate) {
+		this.indate = indate;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+
+	
 
 }
