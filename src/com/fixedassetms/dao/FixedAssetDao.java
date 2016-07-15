ff1f1ddeeb5a6f7795fe6bef1ee47fc8921c63f7
Muse_dreame <450403579@qq.com>
2016-07-15 23:10:51 +0800

    Dao

--- src/com/fixedassetms/dao/FixedAssetDao.java
+++ src/com/fixedassetms/dao/FixedAssetDao.java
@@ -1,5 +1,52 @@
 package com.fixedassetms.dao;
+import java.util.Date;
+import java.util.List;
 
+import com.fixedassetms.entity.FixedAsset;
+/**
+ * 
+ * @author muse
+ *create on 2016-7-15 20:38:04
+ */
 public interface FixedAssetDao {
+	/**
+	 * 增加类别 （CNT means Category and Type 大类和小类）
+	 */
+	public int CNTAdd(Object [] param);
+	/**
+	 * 删除类（CNT means Category and Type 大类和小类）
+	 */
+	public int CNTDel(Object [] param);
+	/**
+	 * 打印所有大类和小类及关系
+	 */
+	public String CNTPrint(Object [] param);
+	/**
+	 * 增加固定资产信息
+	 */
+	public int FixedAssetAdd(FixedAsset FixedAsset);
+	/**
+	 * 删除固定资产信息
+	 */
+	public int FixedAssetDel(FixedAsset FixedAsset);
+	/**
+	 * 按大类浏览固定资产信息
+	 */
+	public List<FixedAsset> FixedAssetReadByC(FixedAsset FixedAsset);
+	/**
+	 * 按小类浏览固定资产信息
+	 */
+	public List<FixedAsset>  FixedAssetReadByT(FixedAsset FixedAsset);
+	/**
+	 * 按资产编号查询精确查找
+	 */
+	public List<FixedAsset>  FixedAssetSerById(FixedAsset FixedAsset);
+	/**
+	 * 按资产类别查询罗列该类别下全部固定资产
+	 */
+	public List<FixedAsset>  FixedAssetSerByCNT(FixedAsset FixedAsset);
+	/**
+	 * 按使用者罗列使用者所拥有全部固定资产
+	 */
 
 }
--- src/com/fixedassetms/dao/impl/FixedAssetDaoImpl.java
+++ src/com/fixedassetms/dao/impl/FixedAssetDaoImpl.java
@@ -1,8 +1,74 @@
 package com.fixedassetms.dao.impl;
 
+import java.util.List;
+
 import com.fixedassetms.dao.BaseDao;
 import com.fixedassetms.dao.FixedAssetDao;
+import com.fixedassetms.entity.FixedAsset;
 
+/**
+ * 
+ * @author muse
+ *create on 2016-7-15 22:57:19
+ */
 public class FixedAssetDaoImpl extends BaseDao implements FixedAssetDao{
+
+	@Override
+	public int CNTAdd(Object[] param) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int CNTDel(Object[] param) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String CNTPrint(Object[] param) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int FixedAssetAdd(FixedAsset FixedAsset) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int FixedAssetDel(FixedAsset FixedAsset) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<FixedAsset> FixedAssetReadByC(FixedAsset FixedAsset) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<FixedAsset> FixedAssetReadByT(FixedAsset FixedAsset) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<FixedAsset> FixedAssetSerById(FixedAsset FixedAsset) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<FixedAsset> FixedAssetSerByCNT(FixedAsset FixedAsset) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	
+	
 
 }
